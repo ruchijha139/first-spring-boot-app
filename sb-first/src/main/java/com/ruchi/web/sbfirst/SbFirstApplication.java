c5ff3f360398bdb3f92fcf31e81c391a7a944534
ruchijha139 <50509205+ruchijha139@users.noreply.github.com>
2019-05-11 12:04:18 +0530

    Add files via upload
    spring boot first comit

---
+++ sb-first/src/main/java/com/ruchi/web/sbfirst/SbFirstApplication.java
@@ -0,0 +1,15 @@
+package com.ruchi.web.sbfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+public class SbFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbFirstApplication.class, args);
+	}
+
+}
